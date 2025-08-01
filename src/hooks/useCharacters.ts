875ff496b4639636e3aa73c4fb286aed67684d4e
amirhossein azarafrooz <amirhosseinazarafrooz@gmail.com>
2025-08-01 11:43:00 +0330

    fix: add type to data in useCharacter

--- src/hooks/useCharacters.ts
+++ src/hooks/useCharacters.ts
@@ -1,20 +1,37 @@
-// src/hooks/useCharacters.ts
 import { useQuery } from '@tanstack/react-query';
 import { graphqlClient } from '../api/client';
 import { GET_CHARACTERS } from '../graphql/queries';
+
+export type Character = {
+  id: string;
+  name: string;
+  status: string;
+  species: string;
+  image: string;
+};
+
+export type CharactersResponse = {
+  characters: {
+    info: {
+      count: number;
+      pages: number;
+    };
+    results: Character[];
+  };
+};
 
 export const useCharacters = (
   page: number,
   filter: { status?: string; species?: string }
 ) => {
-  return useQuery({
+  return useQuery<CharactersResponse['characters']>({
     queryKey: ['characters', page, filter],
     queryFn: async () => {
-      const data = await graphqlClient.request(GET_CHARACTERS, {
-        page,
-        filter,
-      });
-      return data.characters; 
+      const data = await graphqlClient.request<CharactersResponse>(
+        GET_CHARACTERS,
+        { page, filter }
+      );
+      return data.characters;
     },
   });
 };
